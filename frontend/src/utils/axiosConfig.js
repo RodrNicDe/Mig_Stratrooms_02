1429ca907f469b0aae42ae6d6fca7f40e10c4ce3
RodrNicDe <rodrigo.zai@hotmail.com>
2025-06-01 17:12:56 -0600

    Describe brevemente los cambios realizadosrealizado spor algeuin

--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL, // URL base del backend
+  baseURL: "http://localhost:3001", // URL base del backend
   headers: {
     "Content-Type": "application/json",
   },
