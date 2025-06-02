4c36cf88085d020497055e0db2ca089db90be376
RodrNicDe <rodrigo.zai@hotmail.com>
2025-06-02 00:51:55 -0600

    Describe brevemente los cambios realizados

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
 
 const port = process.env.PORT || 3001;
+const host = '0.0.0.0'; // importante para Render
 
-app.listen(port, () => {
-  console.log(`Servidor corriendo en http://localhost:${port}`);
+app.listen(port, host, () => {
+  console.log(`Servidor corriendo en http://${host}:${port}`);
 });
